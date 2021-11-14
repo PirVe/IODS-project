144eec851b3ee0ff0dbef9a86aaa83e49c75b944
Lintulai <93400427+Lintulai@users.noreply.github.com>
2021-11-14 11:52:12 +0200

    working on step 3

--- data/create_learning2014.R
+++ data/create_learning2014.R
@@ -12,3 +12,45 @@ dim(learning2014)
 str(learning2014)
 # Output: a few rows and a few columns with values and value types
 # step 2 completed 
+
+# Creating an analysis dataset
+
+# Accessing the dplyr library
+install.packages("dplyr")
+library(dplyr)
+
+# combining questions for deep, surface and strategic learning
+deep_questions <- c("D03", "D11", "D19", "D27", "D07", "D14", "D22", "D30","D06",  "D15", "D23", "D31")
+surface_questions <- c("SU02","SU10","SU18","SU26", "SU05","SU13","SU21","SU29","SU08","SU16","SU24","SU32")
+strategic_questions <- c("ST01","ST09","ST17","ST25","ST04","ST12","ST20","ST28")
+
+# Selecting the columns from learning2014 and scaling them by taking the mean
+deep_columns <- select(learning2014, one_of(deep_questions))
+learning2014$deep <- rowMeans(deep_columns)
+
+surface_columns <- select(learning2014, one_of(surface_questions))
+learning2014$surf <- rowMeans(surface_columns)
+
+strategic_columns <- select(learning2014, one_of(strategic_questions))
+learning2014$stra <- rowMeans(strategic_columns)
+
+# Scaling the column "Attitude" by dividing it by the sum of 10 questions to get it back to Likert scale 1-5
+learning2014$Attitude <- learning2014$attitude / 10
+
+# Checking current column names in learning2014
+colnames(learning2014)
+
+# choosing the columns to keep and creating a new dataset with them
+keep_columns <- c("gender","Age","Attitude", "deep", "stra", "surf", "Points")
+lrn2014 <- select(learning2014, one_of(keep_columns))
+
+# Checking current column names in lrn2014
+colnames(lrn2014)
+
+# renaming columns Age, Attitude and Points
+colnames(lrn2014)[2] <- "age"
+colnames(lrn2014)[3] <- "attitude"
+colnames(lrn2014)[7] <- "points"
+
+
+
