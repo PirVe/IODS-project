cfdc7f69cec242134b58e708816f6c49b63f0653
Lintulai <93400427+Lintulai@users.noreply.github.com>
2021-11-28 10:40:05 +0200

    step 2 done

---
+++ data/create_human.R
@@ -0,0 +1,8 @@
+# Piritta Vesaniemi, 28.11.2021, Rscript file for data wrangling exercise
+
+# Read the data into RStudio
+hd <- read.csv("http://s3.amazonaws.com/assets.datacamp.com/production/course_2218/datasets/human_development.csv", stringsAsFactors = F)
+gii <- read.csv("http://s3.amazonaws.com/assets.datacamp.com/production/course_2218/datasets/gender_inequality.csv", stringsAsFactors = F, na.strings = "..")
+
+
+
