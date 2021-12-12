1a4735e2823f3597b3c643ca9b638af96481cdd2
Lintulai <93400427+Lintulai@users.noreply.github.com>
2021-12-12 16:06:01 +0200

    some comments added to step 1

--- data/meet_and_repeat.R
+++ data/meet_and_repeat.R
@@ -13,25 +13,34 @@ RATS <- read.table("https://raw.githubusercontent.com/KimmoVehkalahti/MABS/maste
 # Saving the datasets as files to my project:
 rats <- RATS
 path <- "C:/Users/35840/OneDrive - University of Helsinki/IODS/IODS-project/data"
-write.csv(rats, file.path(path, "rats.csv"), row.names=TRUE)
+write.csv(rats, file.path(path, "rats.csv"))
 
 bprs <- BPRS
-write.csv(bprs, file.path(path, "bprs.csv"), row.names=TRUE)
+write.csv(bprs, file.path(path, "bprs.csv"))
 
 # Look at the (column) names of BPRS
 names(BPRS)
+# The column names are treatment, subject, week0-8
 
 # Look at the structure of BPRS
 str(BPRS)
+# there are 40 observations of 11 variables, all type "int"
 
 # Print out summaries of the variables
 summary(BPRS)
+# The mean each week goes down, starting from 48 and ending with 31.43. 
 
 # Look at the (column) names of RATS
 names(RATS)
+# The names are ID, Group,  WD1,  WD8, WD15, WD2, WD29, WD36, WD43, WD44, WD50, WD57, WD64 
 
 # Look at the structure of BPRS
 str(RATS)
+# There are 16 observations of 13 variables
 
 # Print out summaries of the variables
 summary(RATS)
+# Just as with the BPRS dataset, the summaries of the first two variables do not really tell anything, because they signify the individual rat and group.
+
+
+
